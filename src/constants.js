3332c07d46fecb734b143259af4d667ef9b1036a
MIVD <mi.vandeurzen@gmail.com>
2021-02-02 11:05:04 +0100

    change site title & icon

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,4 @@
+//For demonstration purposes these api keys are included in the repository
+//Normally I wouldn't include these and put them in a .env file instead
 export const GEOAPIFY_API_KEY = '4680392dcd4944afad13f1d18834846e';
 export const ORS_API_KEY = '5b3ce3597851110001cf6248daa458bb7e59428895b9711f573dc9c1'
